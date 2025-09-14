077e147bd9f86e580d77ca4534204d9472636d4c
Jia-A <agrawaljiya2011@gmail.com>
2025-09-14 12:12:38 +0530

    feat: create Auth component and enhance AuthForm styling; implement responsive layout and improve error message display

--- quill-frontend/src/components/AuthForm.tsx
+++ quill-frontend/src/components/AuthForm.tsx
@@ -1,4 +1,4 @@
-'use client'
+"use client";
 import { useForm } from "react-hook-form";
 import Button from "../atoms/Button";
 import Input from "../atoms/Input";
@@ -11,15 +11,15 @@ import { useRouter } from "next/navigation";
 
 const AuthForm = () => {
   const [isSignupForm, setIsSignupForm] = useState(true);
-  const router = useRouter()
+  const router = useRouter();
   const {
     handleSubmit: handleSubmitSignup,
     register: registerSignup,
     getValues: getSignupValues,
     reset: resetSignup,
-    formState: { errors : signupErrors }
+    formState: { errors: signupErrors },
   } = useForm<SignupInput>({
-    resolver : zodResolver(signupSchema),
+    resolver: zodResolver(signupSchema),
     defaultValues: { name: "", email: "", password: "" },
   });
 
@@ -28,10 +28,11 @@ const AuthForm = () => {
     register: registerSignin,
     getValues: getSigninValues,
     reset: resetSignin,
-    formState: { errors : signinErrors }
-  } = useForm<SigninInput>({ 
-    resolver : zodResolver(signinSchema),
-    defaultValues: { email: "", password: "" } });
+    formState: { errors: signinErrors },
+  } = useForm<SigninInput>({
+    resolver: zodResolver(signinSchema),
+    defaultValues: { email: "", password: "" },
+  });
 
   const signupHandler = async () => {
     const { name, email, password } = getSignupValues();
@@ -42,8 +43,9 @@ const AuthForm = () => {
     };
     console.log("Signup payload:", payload);
     try {
-      await signupAction(payload);
+      const response = await signupAction(payload);
       resetSignup();
+      localStorage.setItem("customer", JSON.stringify(response.user));
       router.push("/blogs");
       console.log("Signup successful");
     } catch (error) {
@@ -58,8 +60,9 @@ const AuthForm = () => {
       password,
     };
     try {
-      await signinAction(payload);
+      const response = await signinAction(payload);
       resetSignin();
+      localStorage.setItem("customer", JSON.stringify(response.userExists));
       router.push("/blogs");
       console.log("Signup successful");
     } catch (error) {
@@ -81,7 +84,7 @@ const AuthForm = () => {
           </span>
           <span
             className="underline font-bold cursor-pointer"
-            onClick={() => setIsSignupForm(prev=> !prev)}
+            onClick={() => setIsSignupForm((prev) => !prev)}
           >
             {isSignupForm ? "Signin here" : "Signup here"}
           </span>
@@ -91,23 +94,48 @@ const AuthForm = () => {
           <form onSubmit={handleSubmitSignup(signupHandler)}>
             <span className="flex flex-col gap-3 w-[350px] mt-4">
               <Input label="Fullname" register={registerSignup("name")} />
-              {signupErrors.name && <p className="text-red-500 text-sm">{signupErrors.name.message}</p>}
+              {signupErrors.name && (
+                <p className="text-red-500 text-sm">
+                  {signupErrors.name.message}
+                </p>
+              )}
               <Input label="Email" register={registerSignup("email")} />
-              {signupErrors.email && <p className="text-red-500 text-sm">{signupErrors.email.message}</p>}
+              {signupErrors.email && (
+                <p className="text-red-500 text-sm">
+                  {signupErrors.email.message}
+                </p>
+              )}
               <Input label="Password" register={registerSignup("password")} />
-              {signupErrors.password && <p className="text-red-500 text-sm">{signupErrors.password.message}</p>}
+              {signupErrors.password && (
+                <p className="text-red-500 text-sm">
+                  {signupErrors.password.message}
+                </p>
+              )}
             </span>
-            <Button label="Signup" type="submit" />
+            <Button label="Signup" type="submit" variant="primary" className="mt-6 w-full md:w-1/2 justify-center" />
           </form>
         ) : (
           <form onSubmit={handleSubmitSignin(signinHandler)}>
             <span className="flex flex-col gap-3 w-[350px] mt-4">
               <Input label="Email" register={registerSignin("email")} />
-              {signinErrors.email && <p className="text-red-500 text-sm">{signinErrors.email.message}</p>}
+              {signinErrors.email && (
+                <p className="text-red-500 text-sm">
+                  {signinErrors.email.message}
+                </p>
+              )}
               <Input label="Password" register={registerSignin("password")} />
-               {signinErrors.password && <p className="text-red-500 text-sm">{signinErrors.password.message}</p>}
+              {signinErrors.password && (
+                <p className="text-red-500 text-sm">
+                  {signinErrors.password.message}
+                </p>
+              )}
             </span>
-            <Button label="Signin" type="submit" />
+            <Button
+              label="Signin"
+              type="submit"
+              variant="primary"
+              className="mt-6"
+            />
           </form>
         )}
       </div>
