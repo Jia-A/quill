2d2b9d1d8a4499eff2d47e0715784cdad6892285
Jia-A <agrawaljiya2011@gmail.com>
2026-09-02 14:28:47 +0530

    feat: improve error handling in signup action

--- quill-frontend/src/actions/authActions.ts
+++ quill-frontend/src/actions/authActions.ts
@@ -4,10 +4,16 @@ import axios from "axios";
 
 export const signupAction = async (payload: SignupInput) => {
   const signupInp = { ...payload, avatar: "" };
-  const response = await axios.post(`${API_URL}/user/signup`, signupInp);
-  if (response.status === 200) {
-    return response.data;
-  } else {
-    throw new Error("Signup failed");
+  try {
+    const response = await axios.post(`${API_URL}/user/signup`, signupInp);
+    return response;
+  } catch (error) {
+    console.error("Error signing up:", error);
+    if (error?.response && error?.response?.data && error.response?.data?.error) {
+      const errorData = error.response.data.error;
+      throw new Error(errorData.message || "Signup failed");
+    } else {
+      throw new Error("Signup failed");
+    }
   }
 };
