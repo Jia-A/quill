ff6174419e605139a9028392ce4cc4b922e0aa13
Jia-A <agrawaljiya2011@gmail.com>
2026-09-06 13:29:26 +0530

    feat: implement user profile retrieval and update actions

---
+++ quill-frontend/src/actions/userActions.ts
@@ -0,0 +1,26 @@
+import { API_URL } from "@/utils/constants";
+import axios from "axios";
+
+export const getUserProfile = async (token) => {
+  try {
+    const response = await axios.get(`${API_URL}/user/me`, {
+      headers: { authorization: token },
+    });
+    return response.data;
+  } catch (err) {
+    console.error("Error fetching user profile:", err.response?.data?.error);
+    throw new Error(err.response?.data?.error?.message || "Something went wrong");
+  }
+};
+
+export const updateUserProfile = async (token, payload) => {
+  try {
+    const response = await axios.put(`${API_URL}/user/me`, payload, {
+      headers: { authorization: token },
+    });
+    return response.data;
+  } catch (err) {
+    console.error("Error updating user profile:", err.response?.data?.error);
+    throw new Error(err.response?.data?.error?.message || "Something went wrong");
+  }
+};
